7f8f2450d0fdbf029df5d3e9c595719331706e41
lies93 <hocini.lies@gmail.com>
2022-04-11 16:08:51 +0200

    Step one done

---
+++ Externals/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const axios = require("axios");
+const { response } = require('express');
+
+const port = 3009;
+app.listen(port,()=>{
+    console.log('server on'+ port);
+})
+
+const data = require('./data.json');
+console.log(data);
+
+app.get('/game-of-thrones/json',(req,res)=>{
+    res.json(data)
+})
+
+
+
+// axios.get("https://thronesapi.com/api/v2/Characters")
+//     .then(response => console.log(response.data))
+
