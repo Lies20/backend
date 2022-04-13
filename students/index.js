87b57c50b179228e3668489d7ca775e5cc4c29e8
lies93 <hocini.lies@gmail.com>
2022-04-13 14:22:31 +0200

    step 2 done

---
+++ students/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const port = 3002;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(port,()=>{
+    console.log('this port is ' + " " + port)
+})
+
+let students = ['lala','lolo']
+
+app.get('/students',(req,res)=>{
+    res.json(students)
+})
+
+app.post('/students', function (req, res) {
+    console.log("result"+ " " +req.body.name);
+    res.send();
+  });
